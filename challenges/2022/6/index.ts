8a698b90835fc5647057a9b27d8b7d7a5e426e0f
Frikkie Snyman <frikkie@relive.cc>
2022-12-06 09:26:09 +0200

    small optimization

--- challenges/2022/6/index.ts
+++ challenges/2022/6/index.ts
@@ -7,6 +7,9 @@ const firstMarkerPosition = (stream: string, count: number): number => {
     const set = new Set();
     for (let i = 0; i < count; i++) {
       set.add(stream[index - i]);
+      if (set.size < i + 1) {
+        break;
+      }
     }
     if (set.size === count) {
       return index;
@@ -23,6 +26,9 @@ const part1 = () => {
 const part2 = () => {
   return firstMarkerPosition(getInput("2022", "6"), 14) + 1;
 };
-
+let start = Date.now();
 console.log(`Solution 1: ${part1()}`);
+console.log("took", Date.now() - start);
+start = Date.now();
 console.log(`Solution 2: ${part2()}`);
+console.log("took", Date.now() - start);
